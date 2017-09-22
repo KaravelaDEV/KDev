3648aaa65125a363b1b28cbe7202f920d676ad68
Jefferon Lima <jefferson.lima@gisplan.com.br>
2017-09-22 14:41:31 -0300

    crud implemented

--- src/main/java/TaskDAO.java
+++ src/main/java/TaskDAO.java
@@ -1,36 +1,92 @@
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 public class TaskDAO {
-    private Task task;
+    private Connection conn;
+    private String url = "jdbc:sqlite::memory:";
 
     public void createDatabase(){
-        String url = "jdbc:sqlite::memory:";
+        String TABLE_TASK_DEFINITION = "CREATE TABLE TASK(ID integer PRIMARY KEY,  NAME text NOT NULL)";
 
-        try (Connection conn = DriverManager.getConnection(url)) {
-            if (conn != null) {
-                DatabaseMetaData meta = conn.getMetaData();
-                System.out.println("The driver name is " + meta.getDriverName());
-                System.out.println("A new database has been created.");
-            }
-
+        try {
+            conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            stmt.execute(TABLE_TASK_DEFINITION);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public void save(Task task){
-        this.createDatabase();
+    public void save(Task task) throws SQLException{
+        try {
+            String sqlInsert = "INSERT INTO TASK(name) VALUES(?)";
+            PreparedStatement pstmt =
+                    conn.prepareStatement(sqlInsert, Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, task.getName());
 
-        this.task = task;
-        this.task.setID(2000);
+            int affectedRows = pstmt.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("Creating task failed, no rows affected.");
+            }
+
+            ResultSet generatedKeys = pstmt.getGeneratedKeys();
+            if (!generatedKeys.next()) {
+                throw new SQLException("Creating task failed, no ID obtained.");
+            }
+            task.setID(generatedKeys.getInt(1));
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            throw new SQLException(e.getMessage());
+        }
     }
 
-    public Task fetch(int ID) {
-        Task task = new Task("Create Course");
-        task.setID(ID);
+    public Task fetchByID(int ID) throws SQLException{
+        Task task = null;
+        try {
+            String sqlQuery = "SELECT ID, NAME FROM TASK WHERE ID = ?";
+
+            PreparedStatement pstmt = conn.prepareStatement(sqlQuery);
+            pstmt.setInt(1, ID);
+            ResultSet rs  = pstmt.executeQuery();
+
+            while (rs.next()) {
+                task = new Task(rs.getString("NAME"));
+                task.setID(rs.getInt("ID"));
+            }
+
+        } catch (SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
+
         return task;
+    }
+
+    public void update(Task task) throws SQLException {
+        try {
+            String sqlUpdate = "UPDATE TASK SET name = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sqlUpdate);
+            pstmt.setString(1, task.getName());
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
+    }
+
+    public void remove(Task task) throws SQLException{
+        try {
+            String sqlDelete = "DELETE FROM TASK WHERE ID = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sqlDelete);
+            pstmt.setInt(1, task.getID());
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
     }
 }
--- src/test/java/TaskDAOTest.java
+++ src/test/java/TaskDAOTest.java
@@ -4,21 +4,65 @@ import static org.junit.Assert.*;
 
 public class TaskDAOTest {
     @Test
-    public void createTask() throws  Exception {
-        Task task = new Task("Create Course");
+    public void createTask() throws Exception {
         TaskDAO taskDAO= new TaskDAO();
+        taskDAO.createDatabase();
+
+        Task task = new Task("Create Course");
         taskDAO.save(task);
 
-        assertEquals(2000, task.getID());
+        assertEquals(1, task.getID());
         assertEquals("Create Course", task.getName());
     }
 
     @Test
-    public void fetchTask() throws  Exception {
+    public void fetchTaskByID() throws Exception {
         TaskDAO taskDAO = new TaskDAO();
-        Task task = taskDAO.fetch(2000);
+        taskDAO.createDatabase();
 
-        assertEquals(2000, task.getID());
+        Task task = new Task("Create Course");
+        taskDAO.save(task);
+
+        task = taskDAO.fetchByID(1);
+
+        assertNotNull(task);
+        assertEquals(1, task.getID());
         assertEquals("Create Course", task.getName());
+    }
+
+    @Test
+    public void updateTask() throws Exception {
+        TaskDAO taskDAO = new TaskDAO();
+        taskDAO.createDatabase();
+
+        Task task = new Task("Create Course");
+        taskDAO.save(task);
+
+        task = taskDAO.fetchByID(1);
+        assertEquals(1, task.getID());
+        assertEquals("Create Course", task.getName());
+
+        task.setName("Course Updated");
+        taskDAO.update(task);
+
+        task = taskDAO.fetchByID(1);
+        assertEquals(1, task.getID());
+        assertEquals("Course Updated", task.getName());
+    }
+
+    @Test
+    public void removeTask() throws Exception {
+        TaskDAO taskDAO = new TaskDAO();
+        taskDAO.createDatabase();
+
+        Task task = new Task("Create Course");
+        taskDAO.save(task);
+
+        task = taskDAO.fetchByID(1);
+        assertEquals(1, task.getID());
+
+        taskDAO.remove(task);
+        task = taskDAO.fetchByID(1);
+        assertNull(task);
     }
 }
